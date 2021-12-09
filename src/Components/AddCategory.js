9b7b7ba2eeb13986a8df12f307356481aa61aa87
CeRuGu97 <cesarchivas72@gmail.com>
2021-12-08 23:50:40 -0600

    ultimo add

--- src/Components/AddCategory.js
+++ src/Components/AddCategory.js
@@ -4,12 +4,13 @@ import PropTypes from 'prop-types';
 export const AddCategory = ({ setCategories }) => {
     
     const [inputValue, setInputValue] = useState('');
+    
     const handleInputChange = (e) => {
         setInputValue(e.target.value)
     }
 
     const handleSubmit = (e) => {
-        e.preventDefault();
+        e.preventDefault(); //con esto no recarga la pagina
         
         if (inputValue.trim().length > 2) {
             setCategories( cats => [inputValue, ...cats] );
